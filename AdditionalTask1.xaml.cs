575458715375215027442462dea2901d65b2b16f
admin <admin@DESKTOP-FA5KDQG>
2024-06-04 16:01:35 +0300

    Add project files.

---
+++ AdditionalTask1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleCalculator_WPF
+{
+    /// <summary>
+    /// Interaction logic for AdditionalTask1.xaml
+    /// </summary>
+    public partial class AdditionalTask1 : Window
+    {
+        private string preferences = "";
+        public AdditionalTask1()
+        {
+            InitializeComponent();
+        }
+
+        private void showPreferencesButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (StackPanel stackPanel in preferencesGrid.Children.OfType<StackPanel>())
+            {
+                foreach (RadioButton radio in stackPanel.Children.OfType<RadioButton>().Where(rb => rb.IsChecked == true))
+                {
+                    preferences += stackPanel.Children.OfType<Label>().First().Content;
+                    preferences += radio.Content + "\n";
+                }
+            }
+            MessageBox.Show(preferences);
+            preferences = "";
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (StackPanel stackPanel in preferencesGrid.Children.OfType<StackPanel>())
+            {
+                foreach(RadioButton radio in stackPanel.Children.OfType<RadioButton>())
+                {
+                    radio.IsChecked = false;
+                }
+            }
+        }
+    }
+}
---
+++ AdditionalTask2.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleCalculator_WPF
+{
+    /// <summary>
+    /// Interaction logic for AdditionalTask2.xaml
+    /// </summary>
+    public partial class AdditionalTask2 : Window
+    {
+        public AdditionalTask2()
+        {
+            InitializeComponent();
+        }
+
+        private void applyButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = colorsStackPanel.Children.OfType<RadioButton>().FirstOrDefault(rb => rb.IsChecked == true);
+            if (radioButton != null)
+            {
+                currentColorTextBlock.Text = radioButton.Content.ToString();
+                currentColorTextBlock.Foreground = SetColor(currentColorTextBlock.Text);
+                userTextBox.Foreground = SetColor(currentColorTextBlock.Text);
+            }
+        }
+
+        private Brush SetColor(string color)
+        {
+            switch (color)
+            {
+                case "Красный":
+                    return Brushes.Red;
+                case "Зелёный":
+                    return Brushes.Green;
+                case "Синий":
+                    return Brushes.Blue;
+                default:
+                    return Brushes.Black;
+            }
+        }
+    }
+}
